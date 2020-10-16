84ff3f6c281b35d8a0651f207442dd0c41562c06
Ayush Solanki <solankiayush6055@gmail.com>
2020-10-16 17:17:06 +0530

    header file added

---
+++ src/components/todoapp/todoheader.vue
@@ -0,0 +1,107 @@
+<template>
+ <div>
+    <header>
+    <div id="status" :class="{bg:bgState}">
+        Logged In:
+        <span v-if="logged">Yes</span>
+        <span v-else>No</span>
+    </div>
+    <nav>
+        <ol>
+            <li v-if="state"  @click=" changeRoute('home')">Home</li>
+            <li @click=" changeRoute('aboutus')">About-us</li>
+            <li @click=" changeRoute('signin')" v-if="visible">Sign-in</li>
+            <li @click=" changeRoute('signup')" v-if="visible">Sign-up</li>
+            <li @click="signOut" v-if="state">Sign-out</li>
+        </ol>
+          
+    </nav>
+  </header>
+ </div>    
+</template>
+
+<script>
+import * as firebase from "firebase/app";
+import "firebase/auth";
+export default {
+     components:{
+  
+  },
+  data () {
+    return {
+      logged:false,
+      state:false,
+      visible:true,
+      underline:false,
+      bgState:false
+  }
+  },
+  methods:{
+   signOut(){
+     try{
+         const data = firebase.auth().signOut();
+         console.log(data);
+         this.$router.replace({name:"signin"})
+     }catch(err){
+       console.log(err);
+     }
+     
+   },
+   //one function for changing the route
+   changeRoute(value){
+     this.$router.replace({name:value})
+   }
+
+},
+created(){
+  firebase.auth().onAuthStateChanged(user =>{
+    this.logged = !!user;
+    this.state = !!user;
+    this.visible = !user;
+    this.underline = !!user;
+    this.bgState= !!user;
+  })
+}
+}
+</script>
+
+<style scoped>
+header{
+  background-color: grey;
+}
+ nav{
+   background-color:grey;
+   color:white;
+ }
+ ol{
+   text-decoration: none;
+   text-align: right;
+   padding:2em;
+ }
+ li{
+   display: inline;
+   font-size:1.3em;
+   padding-right: 2em;
+   padding-bottom: 2em;
+ 
+ }
+a{
+    color:white;
+}
+.underline{
+  text-decoration:underline;
+}
+li:hover{
+  text-decoration: underline;
+}
+#status{
+  width:10%;
+  border: 3px solid black;
+  padding:1.2em;
+  font-size:1.3em;
+}
+.bg{
+  background-color: lightgray;
+  color: red;
+}
+</style>
