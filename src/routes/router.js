4043efaefd28e9dceee47eae272a64f6e638dc9a
Ayush Solanki <solankiayush6055@gmail.com>
2020-10-28 13:21:08 +0530

    Code for restriction added

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,10 +3,20 @@ import todoApp from '../components/todoapp/todo.vue';
 import signUp from '../components/todoapp/todosignup.vue';
 import signIn from '../components/todoapp/todosignin.vue';
 import AboutUs from '../components/todoapp/aboutus.vue';
+import { store } from '../store/store';
 
 export default[
-    {path :"/home",name:"home",component :todoApp},
+    {path :"/home",name:"home",component :todoApp,
+      beforeEnter:(to,from,next)=>{
+       if(store.state.logged == false){
+       next('/');
+       }else{
+           next();
+       }
+     }     
+},
     {path :"/signup",name:"signup",component :signUp},
     {path :"/",name:"signin",component :signIn},
+        
     {path :"/aboutus",name:"aboutus",component :AboutUs}
 ];
